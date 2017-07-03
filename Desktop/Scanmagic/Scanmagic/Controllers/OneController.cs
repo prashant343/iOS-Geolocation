f32492f2c6f3a06afad4a5c5fe794b3a7b35a06f
prashant kaushik <prashant@ovalelabs.com>
2017-07-03 11:56:08 +0530

    first commit

---
+++ Desktop/Scanmagic/Scanmagic/Controllers/OneController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scanmagic.Controllers
+{
+    public class OneController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
---
+++ Desktop/Scanmagic/Scanmagic/Models/model1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scanmagic
+{
+	public class model1
+	{
+		public model1 ()
+		{
+		}
+	}
+}
+
